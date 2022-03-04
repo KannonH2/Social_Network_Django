2a2dbbce1ce3e0393f9124e632e86391333c204e
Fernando Haring <manaxtatu@gmail.com>
2022-03-04 16:25:16 -0300

    Post edit and delete done

--- social/admin.py
+++ social/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import SocialPost, SocialComments, Image
 
-# Register your models here.
+
+admin.site.register(SocialPost)
+admin.site.register(SocialComments)
+admin.site.register(Image)
--- social/views.py
+++ social/views.py
@@ -1,3 +1,51 @@
+from multiprocessing import context
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+from django.views.generic.edit import UpdateView, DeleteView
+from django.views.generic.base import View
 from django.shortcuts import render
+from .models import SocialPost
 
-# Create your views here.
+
+class PostDetailView(LoginRequiredMixin, View):
+    def get(self, request, pk, *args, **kwargs):
+        post = SocialPost.objects.get(pk=pk)
+
+        context = {
+            'post': post,
+        }
+        return render(request, 'pages/social/post_detail.html', context)
+
+    def psot(self, request, pk, *args, **kwargs):
+        post = SocialPost.objects.get(pk=pk)
+        context = {
+            'post': post,
+        }
+        return render(request, 'pages/social/post_detail.html', context)
+
+
+
+class PostEditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = SocialPost
+    fields = ['body']
+    template_name = 'pages/social/post_edit.html'
+
+    def get_success_url(self):
+        return reverse_lazy('social:post_detail', kwargs={'pk': self.kwargs['pk']})
+        
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model=SocialPost
+    template_name='pages/social/post_delete.html'
+    success_url = reverse_lazy('home')
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
